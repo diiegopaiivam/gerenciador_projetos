314d041a5d00a6b4c0a15c9170fdc21971f3e6be
Diego Paiva <diego.paiva@imobsoft.local>
2020-03-18 13:51:59 -0300

    Criando model abstrato

--- src/QueryBuilder.php
+++ src/QueryBuilder.php
@@ -2,18 +2,19 @@
 
 namespace App;
 
-class QueryBuilder {
-
+class QueryBuilder
+{
     private $sql;
     private $bind = [];
 
-    public function select(string $table){
+    public function select(string $table)
+    {
         $this->sql = "SELECT * FROM `{$table}`";
         return $this;
     }
 
-    public function insert(string $table, array $data){
-
+    public function insert(string $table, array $data)
+    {
         $sql = "INSERT INTO `{$table}` (%s) VALUES (%s)";
 
         $columns = array_keys($data);
@@ -25,20 +26,47 @@ class QueryBuilder {
         return $this;
     }
 
-    public function update(string $table, array $data){
+    public function update(string $table, array $data)
+    {
+        $sql = "UPDATE `{$table}` SET %s";
 
-        $this->sql = "UPDATE `{$table}` SET name = ?";
+        $columns = array_keys($data);
+
+        foreach ($columns as &$column) {
+            $column = $column . '=?';
+        }
+
+        $this->bind = array_values($data);
+        $this->sql = sprintf($sql, implode(', ', $columns));
+
         return $this;
     }
 
-    public function delete(string $table){
+    public function delete(string $table)
+    {
         $this->sql = "DELETE FROM `{$table}`";
         return $this;
-
     }
 
-    public function where(array $coditions){
+    public function where(array $conditions)
+    {
+        if ($conditions == []) {
+            return $this;
+        }
 
+        if (!$this->sql) {
+            throw new \Exception("selete(), update() or delete() is required before where() method");
+        }
+
+        $columns = array_keys($conditions);
+
+        foreach ($columns as &$column) {
+            $column = $column . '=?';
+        }
+
+        $this->bind = array_merge($this->bind, array_values($conditions));
+        $this->sql .= ' WHERE ' . implode(' and ', $columns);
+        return $this;
     }
 
     public function getData() :\stdClass
