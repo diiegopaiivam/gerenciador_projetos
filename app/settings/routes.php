61dcfdf2db94cdb3a6c536aa84eb5691e5f81f6d
Diego Paiva <diego.paiva@imobsoft.local>
2020-03-18 10:14:30 -0300

    Finalizando métodos de inserção e leitura de dados

--- app/settings/routes.php
+++ app/settings/routes.php
@@ -8,4 +8,8 @@ $router->add('GET', '/', function() use ($container){
     return 'Estamos aqui na homepage';
 });
 
+$router->add('GET', '/users', '\Config\Controllers\UserController::index');
 $router->add('GET', '/users/(\d+)', '\Config\Controllers\UserController::show');
+$router->add('POST', '/users', '\Config\Controllers\UserController::create');
+$router->add('PUT', '/users/(\d+)', '\Config\Controllers\UserController::update');
+$router->add('DELETE', '/users/(\d+)', '\Config\Controllers\UserController::delete');
--- app/src/Controllers/UserController.php
+++ app/src/Controllers/UserController.php
@@ -6,12 +6,33 @@ use Config\Models\Users;
 
 class UserController {
 
+    public function index($container, $request){
+        
+    $user = new Users($container);
+    return $user->all();
+       
+    }
+
     public function show($container, $request){
         
         $user = new Users($container);
-        $user->create(['name' => 'Diego Paiva']);
         return $user->get($request->attributes->get(1));
        
+    }
+
+    public function create($container, $request){
+
+        $user = new Users($container);
+        return $user->create($request->request->all());
+    
+    }
+
+    public function update(){
+       
+    }
+
+    public function delete(){
+
     }
 
 }
